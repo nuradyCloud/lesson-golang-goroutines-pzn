977a03195f56a4182b80a9e2d771c3fd695e1e3f
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-06 09:26:13 +0700

    create goroutine using channel for only in and out

--- channel_test.go
+++ channel_test.go
@@ -42,3 +42,23 @@ func TestChannelAsParam(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Nur Ady Pamungkas"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}

6d944e47494b59b80e4f9ac5fe086639f9559410
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-06 09:09:59 +0700

    create goroutine using channel as param

--- channel_test.go
+++ channel_test.go
@@ -25,3 +25,20 @@ func TestCreateChannel(t *testing.T) {
 	time.Sleep(5 * time.Second)
 
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Nur Ady Pamungkas"
+}
+
+func TestChannelAsParam(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}

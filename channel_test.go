d9063bd94768c81781a22f14334ba0115c41ecba
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-07 11:21:38 +0700

    create goroutine case race condition

--- channel_test.go
+++ channel_test.go
@@ -149,3 +149,22 @@ func TestDefaultSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				x = x + 1
+				x = 1000 + 1
+				x = 1000 + 1
+				x = 1000 + 1
+				x = 1000 + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}

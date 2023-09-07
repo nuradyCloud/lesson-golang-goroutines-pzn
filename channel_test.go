16ee56f5a1590927e0d691a0de231a57750941e3
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-07 10:49:00 +0700

    create goroutine using range channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package lesson_golang_goroutines_pzn
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -80,5 +81,22 @@ func TestBufferedChannel(t *testing.T) {
 	}()
 
 	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data ", data)
+	}
+
 	fmt.Println("Done")
 }

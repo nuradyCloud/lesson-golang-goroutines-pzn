4f4d02f41d02bb76559dceb2ca714ac20a5ec347
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-07 11:00:42 +0700

    create goroutine using select channel

--- channel_test.go
+++ channel_test.go
@@ -100,3 +100,27 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Done")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channnel 2", data)
+			counter++
+
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}

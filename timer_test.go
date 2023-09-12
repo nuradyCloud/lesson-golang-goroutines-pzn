4ed174a86f29c91f5d347155ede5c741cde88264
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-12 16:40:44 +0700

    create goroutine case timer package

---
+++ timer_test.go
@@ -0,0 +1,37 @@
+package lesson_golang_goroutines_pzn
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	timer := time.NewTimer(5 * time.Second)
+
+	time := <-timer.C
+
+	fmt.Println(time)
+}
+
+func TestTimerAfter(t *testing.T) {
+	channel := time.After(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <-channel
+	fmt.Println(time)
+}
+
+func TestAfterFunc(t *testing.T) {
+	group := sync.WaitGroup{}
+	group.Add(1)
+
+	time.AfterFunc(5*time.Second, func() {
+		fmt.Println(time.Now())
+		group.Done()
+	})
+
+	group.Wait()
+
+}

ce971a1ba7a0499aba49a2ad4dbc5d5eadc23917
ady.xcloud7 <ady.xcloud7@gmail.com>
2023-09-12 10:52:59 +0700

    create goroutine case pool

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package lesson_golang_goroutines_pzn
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Nur")
+	pool.Put("Ady")
+	pool.Put("Pamungkas")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+}
